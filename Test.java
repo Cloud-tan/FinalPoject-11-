e22b696e7de09823fb790648effa79a062747cb3
Cloudt <maranathajeremy@gmail.com>
2025-10-02 15:16:43 +0700

    changes test.java file

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
+
+;
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("Haiiii, inin baris kode pertamaa");
         System.out.println("Jeremy doank");
+        System.out.println("Nabel jahat");
     }
 }
